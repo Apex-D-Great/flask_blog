f2b839656526a0dffa55d41fc10f7d79aa0b9d36
Apex-D-Great <apelogunemmanuel5@gmail.com>
2022-09-14 03:48:00 +0100

    update

--- app/model.py
+++ app/model.py
@@ -14,7 +14,6 @@ class User(db.Model, UserMixin):
     password_hash = db.Column(db.String(200), nullable=False)
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
     post = db.relationship("Post", backref="poster", lazy=True)
-    # profile_pic = db.Column(db.String(200), nullable=True)
     about = db.Column(db.Text(), nullable=True)
 
     @property
