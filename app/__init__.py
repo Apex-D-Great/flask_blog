313e22d0a93432f849ffc818daa48592de8d72ad
Apex-D-Great <apelogunemmanuel5@gmail.com>
2022-09-14 03:59:10 +0100

    update

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,7 @@ load_dotenv()
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:apexdgreat@localhost/users'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://czfzleixetfgcr:b82cad327ed9cd7be904d0e54f70ea60e307b0b458ae83e2f6c5b1b37d11a5a3@ec2-34-200-205-45.compute-1.amazonaws.com:5432/da7okhkih9p37o'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 db = SQLAlchemy(app)
 # cke editor(rich editor)
